9c08a4e9b59a51c1ed8bb414c5c247142bb57486
Izzy Bolt <izzy@VIV-ID-MBA-04.local>
2020-10-12 10:33:53 +0100

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+// import all required modules for express
+// npm i express
+//npm i express-handlebars
+//npm i body-parser
+
 const express = require('express');
 const hbs = require('express-handlebars');
 const getWeather = require('./lib/getWeather')
+// importing the module that we made in lib
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
@@ -14,6 +20,7 @@ app.engine('.hbs', hbs({
     extname: '.hbs',
     defaultLayout: 'layout',
 }));
+//creating a view engine - lets us display data only availabl on the server
 //building the engine
 //creates a config 
 app.set('view engine', '.hbs');
@@ -25,44 +32,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
-
-// app.get('/', async (req, res) => {
-//     let data = await getWeather();
-//     // let location = data.weather[5].name
-//     let desc = data.weather[0].description
-//     console.log(data);
-//     res.render('index', {submit: 'find out!', title: 'enter your location to find out!', desc});
-// })
+// tells express that bodyparser exists and to use it , allows us to access form data
 
 app.get('/', async (req, res) => {
-    // let desc = data.weather[1].description
     let data = await getWeather();
-
-    // let desc = data.weather[0].description
-    // console.log(data.weather[0].description)
     console.log(data)
     res.render('index', {title: 'enter your location to find out more!'})
 })
 
 
 app.post('/', async (req, res) => {  
-
-    // let desc =  data.weather.description
-    // res.write('index', {name, desc})
-    
+    // we can write any JS in these functions   
     let data = await getWeather(req.body.cityInput, req.body.countryInput) 
     // let desc = data.weather[0].description
     res.render('index', {desc: data.weather[0].description, type: data.weather[0].main});
 })
 
-
-// submitData = (event) => {
-//     app.post()
-// };
+//rules that decide what the user can do 
+//you can use two res.renders but can only have two values within res.render
+// servers can't resoibd more than once per client request
 
 
 
 app.listen(3000, () => {
+    //we need to tell express that the server is running and listening to a port 
     console.log('Server listening on port 3000');
     console.log(__dirname);
 })
